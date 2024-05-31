3793eee918d2b2f997168653ff487e9deb821837
ScoobyDenis <denis_pavlov_20155@mail.ru>
2024-05-31 19:13:04 +0400

    check all changes

--- data_bases/connect_data_base.py
+++ data_bases/connect_data_base.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 DB_NAME = "users.db"
-ADMINS = [1372933011, 1088508317, 364640169]
+ADMINS = [1372933011,1088508317, 364640169]
 
 def connect_db(db_name):
     connect = sqlite3.connect(db_name)
